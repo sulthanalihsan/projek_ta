7dc088b0c53a89faa60ab5487731451e0b31f284
sulthanalihsan <sulthanalihsan5@gmail.com>
2020-07-04 20:34:50 +0800

    fix dependencies

--- lib/views/akun/tentangKami.dart
+++ lib/views/akun/tentangKami.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TentangKami extends StatefulWidget {
   @override
@@ -24,8 +25,51 @@ class _TentangKamiState extends State<TentangKami> {
               SizedBox(
                 height: 20.0,
               ),
-              Text("Dokter Sepatu Apps"),
-              Text("Verson 1.1"),
+              Text("Dokter Sepatu Apps v.1"),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text("by Sulthan"),
+              SizedBox(
+                height: 15.0,
+              ),
+              Text(
+                "Folow Me",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              GestureDetector(
+                  child: Text("Github",
+                      style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.blue)),
+                  onTap: () {
+                    launch('https://www.github.com/sulthanalihsan');
+                  }),
+              SizedBox(
+                height: 2.0,
+              ),
+              GestureDetector(
+                  child: Text("Instagram",
+                      style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.blue)),
+                  onTap: () {
+                    launch('https://www.instagram.com/sulthanalihsan');
+                  }),
+              SizedBox(
+                height: 2.0,
+              ),
+              GestureDetector(
+                  child: Text("Linkedin",
+                      style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.blue)),
+                  onTap: () {
+                    launch('https://www.linkedin.com/in/sulthanalihsan/');
+                  }),
             ],
           ),
         ),
@@ -33,4 +77,3 @@ class _TentangKamiState extends State<TentangKami> {
     );
   }
 }
-
